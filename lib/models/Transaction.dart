83b0179b08121889bdaddf95a7e694a35893e84d
rafaela <rafaela.augusto@somei.com.vc>
2022-12-16 16:48:48 -0300

    Create new class

---
+++ lib/models/Transaction.dart
@@ -0,0 +1,14 @@
+class Transaction {
+  Transaction(
+    this.id, {
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+}
+ 
