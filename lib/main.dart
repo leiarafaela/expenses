6ce5e54e11c80c5324908e08989dfc3badc4ae4b
rafaela <rafaela.augusto@somei.com.vc>
2022-12-16 16:49:25 -0300

    Add expenses cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,23 +22,61 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _transactions = [
+      Transaction(
+        't1',
+        title: 'Conta de Luz',
+        value: 120.20,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        't2',
+        title: 'Jaqueta',
+        value: 150.00,
+        date: DateTime.now(),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Despesas')),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            width: double.infinity,
             child: const Card(
               child: Text('Inicio'),
               color: Colors.amber,
               elevation: 5,
             ),
           ),
-          const Card(
-            child: Text('Inicio'),
-          ),
+          Column(
+              children: _transactions.map(
+            (tr) {
+              return Card(
+                  child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Text(tr.title),
+                      Text(
+                        tr.date.toString(),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    padding: const EdgeInsets.all(10),
+                    //decoration: BoxDecoration(border: Border.all(width: 1)),
+                    child: Text(tr.value.toString(), style: TextStyle(),),
+                  ),
+                ],
+              ));
+            },
+          ).toList()),
         ],
       ),
     );
